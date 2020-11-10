dd3ec41cd54d0fa735765270b5ad2ec2989bbe7e
Steven Kang <kangweibaby@163.com>
2020-11-10 15:46:51 +0800

    update config

---
+++ tests/compare.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+    int i = -1;
+    unsigned int j = 4;
+    bool flag = i < j;
+    std::cout << flag << std::endl;
+    return 0;
+}
